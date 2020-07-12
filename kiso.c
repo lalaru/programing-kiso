6fe24e9a8304ae5d589c858d5c7ad15092bbac74
lalaru <63771327+lalaru@users.noreply.github.com>
2020-07-12 15:37:41 +0900

    Finish kiso.c

--- kiso.c
+++ kiso.c
@@ -57,6 +57,72 @@ int main (void){
     return 0;
 }
 
+   /*  問題５　*/
+#include <stdio.h>
+int main (void){
+    int x;
+     
+    printf("整数値を入力しててください: ");
+    scanf("%d",&x);
 
-       
+    if (x%10!=0)
+      printf("%dは10の倍数ではありません",x);
+    
+    else
+      printf("%dは10の倍数です",x);
+    
+    return 0;
+}
 
+   /*   問題6 　*/
+#include <stdio.h>
+int main (void){
+    int x,i;
+    
+    printf("整数値を入力してください：");
+    scanf("%d",&x);
+    
+    for(i=1;i<=x;i++)
+      printf("%d",i);
+    
+    printf("\n");
+
+    return 0;
+}
+
+    /*  問題7  */
+#include <stdio.h>
+int main (void){
+    int i,j,k,x;
+    
+
+    printf("ピラミッドの段数を入力してください：");
+    scanf("%d",&x);
+
+    for(i=1;i<=x;i++){
+        for(j=x-i;j>=0;j--){
+           printf(" ");
+        }
+            for(k=1;k<=i*2-1;k++){
+               printf("*");
+            }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+ /*  問題8  */
+#include <stdio.h>
+int main (void){
+    int a[]={5,4,3,2,1};
+    
+    int i;
+    for(i=0;i<=4;i++){
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+
+    return 0;
+}
+
+          /*finish*/
